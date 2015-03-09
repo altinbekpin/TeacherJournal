c08e0478c8e2f20530c8b9ab32ef87ec002818f7
Gapur <gapur.kassym@gmail.com>
2015-03-10 00:59:23 +0700

    Creating TeacherJournal project

---
+++ TeacherJournal/TeacherJournal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeacherJournal.MvcApplication" Language="C#" %>
---
+++ TeacherJournal/TeacherJournal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeacherJournal.Startup))]
+namespace TeacherJournal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
